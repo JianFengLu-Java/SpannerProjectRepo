57e9cdd180c8c48b0a96b11adc4cb4a51c0d118d
JianFee <LujianfengLLL@gmail.com>
2025-12-23 22:00:33 +0800

    add options

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -2,59 +2,65 @@
 import { GlobalThemeOverrides, NConfigProvider, NThemeEditor } from 'naive-ui'
 import { useTitleStore } from './stores/title'
 import Dragable from './components/Dragable.vue'
+import { ref } from 'vue'
 const titleStore = useTitleStore()
 
+
 const themeOverrides: GlobalThemeOverrides = {
-	Message: {
-		borderRadius: '8px',
-	},
-	Card: {
-		borderRadius: '8px',
-	},
-	Upload: {
-		borderRadius: '100%',
-	},
-	Input: {
-		borderRadius: '8px',
-	},
-	Button: {
-		borderRadiusTiny: '10px',
-		borderRadiusSmall: '10px',
-		borderRadiusMedium: '10px',
-		borderRadiusLarge: '10px',
-	},
-	Dropdown: {
-		borderRadius: '8px',
-	},
-	Badge: {
-		fontSize: '10px',
-		color: '#FF0004FF',
-	},
+  Message: {
+    borderRadius: '8px',
+  },
+  Card: {
+    borderRadius: '8px',
+  },
+  Upload: {
+    borderRadius: '100%',
+  },
+  Input: {
+    borderRadius: '8px',
+  },
+  Button: {
+    borderRadiusTiny: '10px',
+    borderRadiusSmall: '10px',
+    borderRadiusMedium: '10px',
+    borderRadiusLarge: '10px',
+  },
+  Dropdown: {
+    borderRadius: '8px',
+  },
+  Badge: {
+    fontSize: '10px',
+    color: '#FF0004FF',
+  },
 }
+
+
+
 </script>
 
 <template>
-	<n-config-provider :theme-overrides="themeOverrides">
-		<n-theme-editor>
-			<dragable />
-			<div class="h-screen w-screen overflow-hidden relative">
-				<div class="h-full w-full overflow-hidden">
-					<n-message-provider>
-						<router-view />
-					</n-message-provider>
-				</div>
-			</div>
-		</n-theme-editor>
-	</n-config-provider>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-theme-editor>
+      <dragable />
+
+      <div class="h-screen w-screen overflow-hidden relative">
+        <div class="h-full w-full overflow-hidden">
+          <n-message-provider :container-style="{ marginTop: '16px' }" max="5">
+            <router-view />
+          </n-message-provider>
+        </div>
+      </div>
+    </n-theme-editor>
+  </n-config-provider>
 </template>
 
 <style>
 * {
-	user-select: none;
-	-webkit-user-select: none;
-	/* Chrome/Safari */
-	-moz-user-select: none;
-	/* Firefox */
-	-ms-user-select: none;
+  user-select: none;
+  -webkit-user-select: none;
+  /* Chrome/Safari */
+  -moz-user-select: none;
+  /* Firefox */
+  -ms-user-select: none;
 }
 </style>
--- src/renderer/src/views/Home/SideBar.vue
+++ src/renderer/src/views/Home/SideBar.vue
@@ -1,162 +1,106 @@
 <template>
-	<div
-		class="w-[76px] gap-3 h-full flex flex-col items-center justify-between rounded-[14px]"
-	>
-		<!-- 顶部：头像 -->
-		<div class="flex mt-10 flex-col items-center gap-4 cursor-pointer">
-			<n-dropdown
-				trigger="click"
-				placement="right-start"
-				style="
-					width: 140px;
-					border: 1px solid #ccc;
-					border-radius: 10px;
-				"
-				:options="[
-					{
-						render: () => {
-							return h(
-								'div',
-								{
-									class: 'p-2 cursor-pointer hover:bg-gray-100 rounded-md',
-									onClick: handleLogout,
-								},
-								'确认退出登录？',
-							)
-						},
-					},
-					{
-						label: '退出登录',
-						key: 'logout',
-						icon() {
-							return h(NIcon, null, {
-								default: () => h(LogOutOutline),
-							})
-						},
-						props: {
-							onClick: () => {
-								handleLogout()
-							},
-						},
-					},
-				]"
-			>
-				<n-avatar round :src="user.avatarUrl" />
-			</n-dropdown>
-		</div>
-		<div>
-			<div
-				class="text-zinc-800 w-9 h-9 flex justify-center bg-gray-50 items-center rounded-full hover:bg-zinc-300 cursor-pointer"
-			>
-				<n-icon size="20">
-					<Search />
-				</n-icon>
-			</div>
-		</div>
-		<div>
-			<n-dropdown
-				trigger="click"
-				placement="right-start"
-				style="
-					width: 140px;
-					border: 1px solid #ccc;
-					border-radius: 10px;
-				"
-				:options="[
-					{
-						label: '新建聊天',
-						key: 'new-chat',
-						icon() {
-							return h(NIcon, null, {
-								default: () => h(Add),
-							})
-						},
-						onClick: () => {
-							console.log('新建聊天')
-						},
-					},
-					{
-						label: '新建群组',
-						key: 'new-group',
-						icon() {
-							return h(NIcon, null, {
-								default: () => h(Add),
-							})
-						},
-						onClick: () => {
-							console.log('新建群组')
-						},
-					},
-				]"
-			>
-				<div
-					class="text-zinc-800 w-9 h-9 flex justify-center bg-gray-50 items-center rounded-full hover:bg-zinc-300 cursor-pointer"
-				>
-					<n-icon size="20">
-						<Add />
-					</n-icon>
-				</div>
-			</n-dropdown>
-		</div>
+    <div class="w-[76px] gap-3 h-full flex flex-col items-center justify-between rounded-[14px]">
+        <!-- 顶部：头像 -->
+        <div class="flex mt-10 flex-col items-center gap-4 cursor-pointer">
+            <n-dropdown trigger="click" placement="right-start" style="
 
-		<div
-			class="flex-1 w-full flex flex-col items-center gap-1 rounded-md pt-0 p-2"
-		>
-			<!-- 自定义按钮结构 -->
-			<div
-				v-for="item in menus"
-				:key="item.key"
-				:class="[
-					'flex flex-col items-center justify-center gap-1 h-14 w-16 cursor-pointer rounded-lg transition-all',
-					route.name === item.name
-						? ' bg-white text-primary-600'
-						: 'hover:bg-gray-50 ',
-				]"
-				@click="go(item)"
-			>
-				<!-- 图标 -->
-				<n-badge :dot="item.hasMessage" color="red" size="small">
-					<div
-						class="h-8 w-8 flex flex-col gap-1 justify-center items-center"
-					>
-						<n-icon
-							size="18"
-							:color="route.name === item.name ? '#444' : '#aaa'"
-						>
-							<component :is="iconMap[item.icon]" />
-						</n-icon>
-						<!-- 文字 -->
-						<span
-							class="text-[10px] leading-none font-medium"
-							:style="{
-								color:
-									route.name === item.name ? '#444' : '#aaa',
-							}"
-						>
-							{{ item.label }}
-						</span>
-					</div>
-				</n-badge>
-			</div>
-		</div>
-	</div>
+					border: 1px solid #ccc;
+					border-radius: 10px;
+                    
+				" :options="userMenuOptions">
+                <n-avatar round :src="user.avatarUrl" />
+            </n-dropdown>
+        </div>
+        <div>
+            <div
+                class="text-zinc-800 w-9 h-9 flex justify-center bg-gray-50 items-center rounded-full hover:bg-zinc-300 cursor-pointer">
+                <n-icon size="20">
+                    <Search />
+                </n-icon>
+            </div>
+        </div>
+        <div>
+            <n-dropdown trigger="click" placement="right-start" style="
+					width: 140px;
+					border: 1px solid #ccc;
+					border-radius: 10px;
+				" :options="[
+                    {
+                        label: '新建聊天',
+                        key: 'new-chat',
+                        icon() {
+                            return h(NIcon, null, {
+                                default: () => h(Add),
+                            })
+                        },
+                        onClick: () => {
+                            console.log('新建聊天')
+                        },
+                    },
+                    {
+                        label: '新建群组',
+                        key: 'new-group',
+                        icon() {
+                            return h(NIcon, null, {
+                                default: () => h(Add),
+                            })
+                        },
+                        onClick: () => {
+                            console.log('新建群组')
+                        },
+                    },
+                ]">
+                <div
+                    class="text-zinc-800 w-9 h-9 flex justify-center bg-gray-50 items-center rounded-full hover:bg-zinc-300 cursor-pointer">
+                    <n-icon size="20">
+                        <Add />
+                    </n-icon>
+                </div>
+            </n-dropdown>
+        </div>
+
+        <div class="flex-1 w-full flex flex-col items-center gap-1 rounded-md pt-0 p-2">
+            <!-- 自定义按钮结构 -->
+            <div v-for="item in menus" :key="item.key" :class="[
+                'flex flex-col items-center justify-center gap-1 h-14 w-16 cursor-pointer rounded-lg transition-all',
+                route.name === item.name
+                    ? ' bg-white text-primary-600'
+                    : 'hover:bg-gray-50 ',
+            ]" @click="go(item)">
+                <!-- 图标 -->
+                <n-badge :dot="item.hasMessage" color="red" size="small">
+                    <div class="h-8 w-8 flex flex-col gap-1 justify-center items-center">
+                        <n-icon size="18" :color="route.name === item.name ? '#444' : '#aaa'">
+                            <component :is="iconMap[item.icon]" />
+                        </n-icon>
+                        <!-- 文字 -->
+                        <span class="text-[10px] leading-none font-medium" :style="{
+                            color:
+                                route.name === item.name ? '#444' : '#aaa',
+                        }">
+                            {{ item.label }}
+                        </span>
+                    </div>
+                </n-badge>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { NAvatar, NButton, NIcon } from 'naive-ui'
+import { NAvatar, NButton, NIcon, NTag } from 'naive-ui'
 import { ref, onMounted, h } from 'vue'
 import type { Component, render } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import {
-	Chatbubbles,
-	Person,
-	Settings,
-	SearchOutline as Search,
-	LogOutOutline,
-	Add,
+    Chatbubbles,
+    Person,
+    Settings,
+    SearchOutline as Search,
+    LogOutOutline,
+    Add,
 } from '@vicons/ionicons5'
 import { useUserInfoStore } from '@renderer/stores/userInfo'
-import Dragable from '@renderer/components/Dragable.vue'
 
 const user = useUserInfoStore()
 
@@ -171,20 +115,81 @@ const route = useRoute()
  * 菜单项接口
  */
 interface MenuItem {
-	key: string
-	name: string
-	icon: string
-	label?: string
-	hasMessage?: boolean
+    key: string
+    name: string
+    icon: string
+    label?: string
+    hasMessage?: boolean
 }
+
+/**
+ * 用户头像下拉菜单
+ * 
+ */
+
+const userMenuOptions = [
+
+    {
+        key: 'user-info',
+        label: '个人信息',
+        type: 'render',
+        render: () => {
+            return h('div', { class: 'p-4 flex flex-col w-200px' }, [
+                h('div', { class: 'flex ' }, [
+                    h(NAvatar, { class: ' text-lg mb-2', src: user.avatarUrl, size: 62, round: true }),
+                    h('div', { class: 'ml-4 h-full flex flex-col justify-center' }, [
+                        h('div', { class: 'text-lg font-semibold flex items-center text-gray-800' }, [
+                            h('span', null, user.userName),
+                            h(NTag, { class: 'ml-2 text-xs', type: 'success', size: 'small', round: true, }, {
+                                default: () => '董事长',
+                                icon: () => h(NIcon, { size: 12 }, { default: () => h(Person) }),
+                            }),
+                        ]),
+                        h('div', { class: 'text-xs text-gray-500' }, user.email),
+                    ]),
+                ]),
+
+            ])
+        },
+    },
+    {
+        type: 'divider',
+    },
+    {
+
+        label: '我的个人名片',
+        key: 'my-card',
+        props: {
+            onClick: () => {
+                console.log('我的名片与二维码')
+            },
+        },
+    },
+    {
+        type: 'divider',
+    },
+
+    {
+        label: '退出登录',
+        key: 'logout',
+        props: {
+            onClick: () => {
+                handleLogout()
+            },
+        },
+    },
+
+]
+
+
 
 /**
  * 图标映射
  */
 const iconMap: Record<string, Component> = {
-	chat: Chatbubbles,
-	user: Person,
-	setting: Settings,
+    chat: Chatbubbles,
+    user: Person,
+    setting: Settings,
 }
 
 /**
@@ -192,31 +197,31 @@ const iconMap: Record<string, Component> = {
  */
 const menus = ref<MenuItem[]>([])
 onMounted(() => {
-	menus.value = [
-		{
-			key: 'home',
-			name: 'home',
-			icon: 'chat',
-			label: '消息',
-			hasMessage: true,
-		},
-		{ key: 'user', name: 'user', icon: 'user', label: '通讯录' },
-		{ key: 'setting', name: 'setting', icon: 'setting', label: '设置' },
-	]
+    menus.value = [
+        {
+            key: 'home',
+            name: 'home',
+            icon: 'chat',
+            label: '消息',
+            hasMessage: true,
+        },
+        { key: 'user', name: 'user', icon: 'user', label: '通讯录' },
+        { key: 'setting', name: 'setting', icon: 'setting', label: '设置' },
+    ]
 })
 
 function go(item: MenuItem) {
-	if (route.name !== item.name) {
-		router.push({ name: item.name })
-	}
+    if (route.name !== item.name) {
+        router.push({ name: item.name })
+    }
 }
 
 /**
  * 退出登录逻辑
  */
 function handleLogout() {
-	// 调用preload脚本中暴露的接口
-	window.electron.ipcRenderer.send('logout-open-loginWindow')
+    // 调用preload脚本中暴露的接口
+    window.electron.ipcRenderer.send('logout-open-loginWindow')
 }
 </script>
 
